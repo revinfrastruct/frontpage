b37cefae7b1cd94434d7b4c194b2c7fc73756c0a
Johannes Axner <johannes.axner@gmail.com>
2016-11-20 17:21:54 +0100

    Added SASS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
 
 gulp.task('default', function() {
-  // place code for your default task here
+    // place code for your default task here
 });
+
+// Static server
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch("source/scss/*.scss", ['sass']);
+    gulp.watch("source/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src("source/style/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
